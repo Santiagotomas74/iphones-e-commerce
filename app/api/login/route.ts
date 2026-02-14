3652a5f524710dbe72a245c35ff8aee0b2253eec
Santiagotomas74 <chrismar747@gmail.com>
2026-02-14 19:24:41 -0300

    avances 2

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -32,7 +32,7 @@ export async function POST(req: NextRequest) {
         { status: 401 }
       );
     }
-   
+   console.log(isValid ? "Password válido" : "Password inválido");
     // 3. generar JWT
     const token = jwt.sign(
       {
