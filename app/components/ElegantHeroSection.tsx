fc1110ef848aa153e3a2cafe906e061a869eaffa
Santiagotomas74 <chrismar747@gmail.com>
2026-03-05 00:32:36 -0300

    cambios en secciones v2

--- app/components/ElegantHeroSection.tsx
+++ app/components/ElegantHeroSection.tsx
@@ -1,28 +1,37 @@
 export default function IphoneHeroElegant() {
   return (
-    <section className="relative w-full lg:h-[95vh] overflow-hidden">
+    <section className="relative w-full overflow-hidden lg:h-[90vh] h-[85vh] bg-black">
 
-      {/* Fondo optimizado */}
+      {/* Fondo */}
       <div
         className="
-          absolute inset-0 
-          bg-cover 
-          bg-center 
-          sm:bg-center 
-          bg-black
-          bg-[position:60%_center] 
-          lg:bg-center
+          absolute inset-0
+          bg-no-repeat
+          bg-contain
+          sm:bg-cover
+          bg-bottom sm:bg-center
         "
         style={{
+          marginTop: "-150px",
           backgroundImage: 'url("/image/19.png")',
         }}
       />
 
-      {/* Overlay más suave en mobile */}
-      <div className="absolute inset-0 bg-black/50 sm:bg-black/40" />
+      {/* Overlay SOLO en desktop */}
+      <div className=" sm:block absolute inset-0 bg-black/10 lg:bg-black/40" />
 
       {/* Contenido */}
-      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen lg:h-full text-center px-6">
+      <div
+        className="
+          relative z-10
+          flex flex-col items-center
+          justify-start sm:justify-center
+          text-center
+          px-6
+          pt-24 sm:pt-0
+          sm:min-h-screen
+        "
+      >
 
         <p className="text-gray-300 tracking-widest text-xs sm:text-sm uppercase">
           Presentamos
@@ -36,16 +45,6 @@ export default function IphoneHeroElegant() {
           Potencia redefinida. Diseño impecable.
           La experiencia más avanzada jamás creada.
         </p>
-
-        <div className="mt-10 flex gap-8 sm:gap-10 text-white text-base sm:text-lg font-medium">
-          <button className="hover:opacity-70 transition">
-            Más información
-          </button>
-
-          <button className="hover:opacity-70 transition">
-            Comprar
-          </button>
-        </div>
 
       </div>
     </section>
